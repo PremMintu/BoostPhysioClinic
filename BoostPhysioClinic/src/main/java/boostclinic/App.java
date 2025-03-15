d396d716ae56428ba6b0e82ec83267cec24483b0
PremMintu <pv24aak@herts.ac.uk>
2025-03-15 18:12:56 +0000

    created project with mav

---
+++ BoostPhysioClinic/src/main/java/boostclinic/App.java
@@ -0,0 +1,13 @@
+package boostclinic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
